daf2b541747f0c9b90efa289cb5ce8c3cc7dd89a
MichaelCHL <99242085+MichaelCHL@users.noreply.github.com>
2026-04-01 22:37:19 +0800

    finished testing price related functions

--- price/price_main.py
+++ price/price_main.py
@@ -1,23 +1,29 @@
-from price_fetch import get_real_time_price
-from price_move_sig import price_move
+from price.price_fetch import get_real_time_price
+from price.price_move_sig import price_move
 from utils.logger import get_logger
 
 logger = get_logger(__name__)
-def main():
+def main(ticker):
     logger.info("Start testing price_fech and price_main")
     try:
         logger.info("Testing get_real_time_price..")
-        get_real_time_price('ONDS')
+        prices = get_real_time_price(ticker)
+        ticker_pc = prices[f"{ticker}"]["percent_change"]
+        spy_pc = prices['SPY']["percent_change"]
         logger.info("get_real_time_price passed!")
-    except:
-        logger.error("Something's wrong with get_real_time_price!")
+    except TypeError as e:
+        logger.exception(f"Unable to get the percent change: {e}")
+        print("Something's wrong with get_real_time_price!")
+        return
     
     try:
         logger.info("Testing price_move..")
-        price_move()
+        result = price_move(ticker_pc, spy_pc)
+        print(result)
         logger.info("price_move passed!")
-    except:
-        logger.error("Something's wrong with price_move!")
+    except KeyError as e:
+        logger.exception(f"Unable to find the given ticker price:", e)
+        print("Something's wrong with price_move!")
 
 if __name__ == '__main__':
-    main()
+    main('ONDS')
