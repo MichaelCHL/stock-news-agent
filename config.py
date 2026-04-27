bd50aa0f97128fb98f18a84fedafc60504020244
MichaelCHL <99242085+MichaelCHL@users.noreply.github.com>
2026-04-27 22:41:57 +0800

    feat: finished line bot, ready for testing

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+import sys
+from dotenv import load_dotenv
+load_dotenv()
+
 # price_move_sig.py
 POS_PERCENT_THRESHOLD = 2
 NEG_PERCENT_THRESHOLD = -2
@@ -5,3 +10,14 @@ NEG_PERCENT_THRESHOLD = -2
 # web_search.py
 MODEL = "claude-opus-4-6"
 MAX_TOKENS = 1046
+
+# line_bot/handler.py - Line SDK
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET", None)
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+
+if not LINE_CHANNEL_SECRET:
+    print("Spcify LINE_CHANNEL_SECRET as environment variable.")
+    sys.exit(1)
+if not LINE_CHANNEL_ACCESS_TOKEN:
+    print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
+    sys.exit(1)
--- line_bot/handler.py
+++ line_bot/handler.py
@@ -2,9 +2,47 @@ from fastapi import FastAPI, Request, HTTPException
 from linebot.v3.messaging import AsyncApiClient, AsyncMessagingApi, Configuration, ReplyMessageRequest, TextMessage
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from linebot.v3.webhook import WebhookParser
+from orchestrator import orchestrator
+from config import LINE_CHANNEL_SECRET, LINE_CHANNEL_ACCESS_TOKEN
+import asyncio
 
-
+config = Configuration(access_token=LINE_CHANNEL_ACCESS_TOKEN)
 
 app = FastAPI()
+async_api_client = AsyncApiClient(config)
+line_bot_api = AsyncMessagingApi(async_api_client)
+parser = WebhookParser(LINE_CHANNEL_SECRET)
 
-app.post("/")
+@app.post("/callback")
+async def handle_callback(request: Request):
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = await request.body()
+    body = body.decode()
+
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+    
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessageContent):
+            continue
+        
+        ticker_symbol = event.message.text
+        if ticker_symbol:
+            result = await asyncio.to_thread(orchestrator, ticker_symbol)
+        else:
+            result = 'Please enter a valid ticker symbol.'
+        await line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=result)]
+            )
+        )
+
+    return 'OK'
