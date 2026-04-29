9c3417a2654b965c024307a9a60757c12a641921
Michael Work <michaelwork@Michaels-MacBook-Pro.local>
2026-04-29 16:09:07 +0800

    refactor: replace finnhub client with httpx async

--- price/price_fetch.py
+++ price/price_fetch.py
@@ -1,32 +1,31 @@
-from dotenv import load_dotenv
 from utils.logger import get_logger
-from finnhub.exceptions import FinnhubAPIException
 from typing import Dict, Optional
 from config import FINNHUB_API_KEY
 
-import finnhub
-import os
+import httpx
 
 logger = get_logger(__name__)
-finnhub_client = finnhub.Client(api_key=FINNHUB_API_KEY)
 
-def get_real_time_price(symbol: str) -> Optional[Dict[str, Dict[str,float]]]:
+async def get_real_time_price(symbol: str) -> Optional[Dict[str, Dict[str,float]]]:
     try:
         logger.info("Initiate API Request to FinnHub...") 
-        spy = finnhub_client.quote(symbol='SPY')
-        selected = finnhub_client.quote(symbol=symbol)
+        async with httpx.AsyncClient() as client:
+            spy = await client.get(f"https://finnhub.io/api/v1/quote?symbol=SPY&token={FINNHUB_API_KEY}")
+            selected = await client.get(f"https://finnhub.io/api/v1/quote?symbol={symbol}&token={FINNHUB_API_KEY}")
 
-        spy_current = spy["c"]
-        spy_pc = spy["dp"]
-        select_current = selected["c"]
-        select_pc = selected["dp"]
+        spy_current = spy.json()["c"]
+        spy_pc = spy.json()["dp"]
+        select_current = selected.json()["c"]
+        select_pc = selected.json()["dp"]
         logger.info(f"Successfully retrieved price change percentage for SPY and {symbol}")
         return {
                 "SPY": {"current_price": spy_current, "percent_change": spy_pc},
                 f"{symbol}": {"current_price": select_current, "percent_change": select_pc}
                 }
-    except FinnhubAPIException as e:
-        logger.exception(f"API error: {e}") 
+    except httpx.RequestError as e:
+        logger.exception(f"Request error: {e}") 
         return None
-
+    except httpx.HTTPStatusError as e:
+        logger.exception(f"HTTP Status error: {e}") 
+        return None      
 
