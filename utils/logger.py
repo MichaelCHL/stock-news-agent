0f2c66661e3f2d4b4a774536943006bd06408b3d
MichaelCHL <99242085+MichaelCHL@users.noreply.github.com>
2026-05-05 22:16:13 +0800

    feat(logger): add structured logging

--- utils/logger.py
+++ utils/logger.py
@@ -1,34 +1,27 @@
+from pythonjsonlogger import jsonlogger
+
 import logging
 import os
 import datetime
 
 LOG_DIRS = 'logs'
 os.makedirs(LOG_DIRS, exist_ok=True)
-format = '%(asctime)s - %(levelname)s: %(message)s'
-
-logging.basicConfig(
-    filename=os.path.join(LOG_DIRS, datetime.datetime.now().strftime('log_%Y-%m-%d.log')),
-    level=logging.DEBUG,
-    # format=format 
-)
-
-
 
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.propagate = False
+    logger.setLevel(logging.DEBUG)
 
     if not logger.handlers:
         # add logging to terminal
         st_handler = logging.StreamHandler()
-        StreamHandlerformatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-        st_handler.setFormatter(StreamHandlerformatter)
+        formatter = jsonlogger.JsonFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        st_handler.setFormatter(formatter)
         logger.addHandler(st_handler)
 
         file_handler = logging.FileHandler(filename=os.path.join(LOG_DIRS, datetime.datetime.now().strftime('log_%Y-%m-%d.log')),
 )
-        FileHandlerformatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-        file_handler.setFormatter(FileHandlerformatter)
+        file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
     return logging.getLogger(name)
